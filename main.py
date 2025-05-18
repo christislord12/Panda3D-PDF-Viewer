0d71376c044c1db2dc0e653681b6b3b469e91a90
BlenderFan <53404933+christislord12@users.noreply.github.com>
2025-05-18 14:15:01 -0500

    Better PDF viewing capabilities using PyMuPDF and image files

--- main.py
+++ main.py
@@ -2,38 +2,35 @@ from panda3d.core import loadPrcFileData
 loadPrcFileData("", "sync-video t")
 loadPrcFileData("", "show-frame-rate-meter f")
 loadPrcFileData("", "win-size 800 600")
+# support Mac high-res displays
+loadPrcFileData("", "dpi-aware t")
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
-from direct.gui.OnscreenText import OnscreenText
 from direct.gui.DirectGui import *
-from direct.task import Task
 from direct.showbase.DirectObject import DirectObject
 import sys
-import PyPDF2
+import os
+import fitz
+import shutil
+from pathlib import Path
+
+
 class Game(DirectObject):
     def __init__(self):
         base = ShowBase()
-        self.file = "test.pdf"
-        pdfFileObject = open(self.file, "rb")
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
-        self.count = pdfReader.numPages
-        self.forward_speed = 5.0 # units per second
-        self.backward_speed = 2.0
-        self.forward_button = KeyboardButton.ascii_key('w')
-        self.backward_button = KeyboardButton.ascii_key('s')
-        self.space_button = KeyboardButton.space()
-        self.text = TextNode("node name")
         self.pagenumber = 0
-        initaltext = self.readpdf(self.pagenumber)
-        self.text.setText(initaltext)
-        textNodePath = aspect2d.attachNewNode(self.text)
-        textNodePath.setScale(0.5)
-        textNodePath.setPos(0,30,0)
-        textNodePath.reparentTo(render)
-        self.text.setTextColor(0,0,0,1)
-        base.setBackgroundColor(1,1,1)
-        self.text.setWordwrap(35)
-        base.accept("escape",sys.exit)
+        # make temp dir
+        Path("p3d_pdf_viewer_temp").mkdir(parents=True, exist_ok=True)
+        #file name
+        self.file = "test.pdf"
+        self.image = self.loadImageAsPlane("outfile.png")
+        self.image.reparentTo(render)
+        self.image.setPos(8,15,-6)
+        self.image.setScale(0.35)
+        # read the pdf
+        self.readpdf(self.pagenumber)
+        # controls
+        base.accept("escape",self.teardown)
         base.useTrackball()
         base.trackball.node().setPos(-7.55,0,6)
         onebutton = DirectButton(text = ("Next"), scale=.06, command=self.nextpage)
@@ -58,111 +55,103 @@ class Game(DirectObject):
         self.accept("4", self.jump100)
         self.accept("5", self.jump200)
         self.accept("6", self.jump500)
-        taskMgr.add(self.exampleTask, "MyTaskName")
+    def teardown(self):
+        try:
+            shutil.rmtree("p3d_pdf_viewer_temp")
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
+        sys.exit()
     def readpdf(self,page):
-        pdfFileObject = open(self.file, "rb")
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
-        count = pdfReader.numPages
-        page = pdfReader.getPage(page)
-        pagge = page.extractText()
-        return pagge
+        pdffile = "test.pdf"
+        doc = fitz.open(pdffile)
+        page = doc.load_page(page)  # number of page
+        pix = page.get_pixmap(dpi=600)
+        output = "p3d_pdf_viewer_temp/outfile" + str(self.pagenumber) + ".png"
+        pix.save(output)
+        doc.close()
+        # now render it
+        base.trackball.node().setPos(-7.55,0,6)
+        self.image.removeNode();
+        self.image = self.loadImageAsPlane("p3d_pdf_viewer_temp/" + "outfile" + str(self.pagenumber) + ".png")
+        self.image.reparentTo(render)
+        self.image.setPos(8,15,-6)
+        self.image.setScale(0.35)
     def previouspage(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber-1
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def nextpage(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+1
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump10(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+10
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump25(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+25
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump50(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+50
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump100(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+100
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump200(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+200
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
+            self.readpdf(self.pagenumber)
     def jump500(self):
         base.trackball.node().setPos(-7.55,0,6)
         self.pagenumber=self.pagenumber+500
         try :
-            initaltext = self.readpdf(self.pagenumber)
-            self.text.setText(initaltext)
+            self.readpdf(self.pagenumber)
         except IndexError:
-            initaltext = self.readpdf(self.count-1)
-            self.text.setText(initaltext)
             self.pagenumber=self.count-1
-    def exampleTask(self,task):
-        base.trackball.node().setPos(-7.55, 0, base.trackball.node().getZ())
-        base.trackball.node().setHpr(0, 0, 0)
-        speed = 0.0
-        is_down = base.mouseWatcherNode.is_button_down
-        if is_down(self.forward_button):
-            speed += self.forward_speed
-            y_delta = -5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        if is_down(self.backward_button):
-            speed -= self.backward_speed
-            y_delta = 5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        if is_down(self.space_button):
-            speed -= self.backward_speed
-            y_delta = 5 * globalClock.get_dt()
-            base.trackball.node().set_z(base.trackball.node().getZ() + y_delta)
-        return task.cont
+            self.readpdf(self.pagenumber)
+    def loadImageAsPlane(self, file, yresolution = 600):
+        tex = loader.loadTexture(file)
+        tex.setBorderColor(Vec4(0,0,0,0))
+        tex.setWrapU(Texture.WMBorderColor)
+        tex.setWrapV(Texture.WMBorderColor)
+        cm = CardMaker("filepath" + str(self.pagenumber) + ' card')
+        cm.setFrame(-tex.getOrigFileXSize(), tex.getOrigFileXSize(), -tex.getOrigFileYSize(), tex.getOrigFileYSize())
+        card = NodePath(cm.generate())
+        card.setTexture(tex)
+        card.setScale(card.getScale()/ yresolution)
+        card.flattenLight()
+        return card
+    
 fgame = Game()
 base.run()
